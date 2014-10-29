582d73a84d46f35d15044699b0adb161b4749ba6
Alex Mandel <tech@wildintellect.com>
2014-10-28 17:24:23 -0700

    Starting project

---
+++ pre.sh
@@ -0,0 +1,7 @@
+#Installing needed libraries to make the scripts work
+sudo apt-get install build-essential liblensfun-dev python-opencv python-pil
+sudo pip install lensfunpy
+
+#for Python3, opencv would need to be from source on Ubuntu
+sudo apt-get install python3-pil
+sudo pip3 install lensfunpy
---
+++ readme.md
@@ -0,0 +1,3 @@
+Zap-distortion
+----
+A Script to batch correct photo distortion for a GoPro Hero2 using the Lenfun database. Should be usable for any camera in the lensfun db.
