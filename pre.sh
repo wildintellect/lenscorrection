faa41dc31b2edd39e582c7cb43f9cf2b7068a05c
Alex <tech@wildintellect.com>
2014-10-29 14:36:43 -0700

    Ubuntu 12.04 needs a newer lensfun than the stock

--- pre.sh
+++ pre.sh
@@ -4,6 +4,11 @@ sudo apt-get install build-essential liblensfun-dev python-opencv python-pil
 sudo pip install lensfunpy
 
 #Ubuntu 12.04
+#How I backported lenfun
+backportpackage -s trusty -u ppa:wildintellect/wildintellect lensfun
+#But you can just use my ppa
+sudo apt-add-repositorty ppa:wildintellect/wildintellect
+sudo apt-get update
 sudo apt-get install build-essential liblensfun-dev python-opencv
 sudo pip install pillow
 sudo pip install lensfunpy
@@ -11,3 +16,7 @@ sudo pip install lensfunpy
 #for Python3, opencv would need to be from source on Ubuntu
 #sudo apt-get install python3-pil
 #sudo pip3 install lensfunpy
+
+#How I backported lenfun
+backportpackage -s trusty -u ppa:wildintellect/wildintellect lensfun
+
